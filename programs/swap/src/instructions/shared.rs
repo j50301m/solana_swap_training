4deaedd78ebd212f679a86d580bf3ec941b65226
jasonYu <j50301m@gamil.com>
2024-11-12 18:17:42 +0800

    refactor: Refactor transfer_token function in shared.rs

--- programs/swap/src/instructions/shared.rs
+++ programs/swap/src/instructions/shared.rs
@@ -1,31 +1,25 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{token::{transfer_checked, TransferChecked}, token_interface::{Mint, TokenAccount, TokenInterface}};
-
-
-
-
+use anchor_spl::{
+    token::{transfer_checked, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 pub fn transfer_token<'info>(
-    from: &InterfaceAccount<'info,TokenAccount>,
-    to: &Interface<'info,TokenAccount>,
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &Interface<'info, TokenAccount>,
     amount: &u64,
-    mint: InterfaceAccount<'info,Mint>,
+    mint: InterfaceAccount<'info, Mint>,
     authority: &Signer<'info>,
-    token_program: &Interface<'info,TokenInterface>
-) -> Result<()>{
+    token_program: &Interface<'info, TokenInterface>,
+) -> Result<()> {
     let transfer_accounts_options = TransferChecked {
         from: from.to_account_info(),
         to: to.to_account_info(),
         mint: to.to_account_info(),
-        authority: authority.to_account_info(), 
+        authority: authority.to_account_info(),
     };
 
-    let cpi_context = CpiContext::new(
-        token_program.to_account_info(),
-        transfer_accounts_options,
-    );
+    let cpi_context = CpiContext::new(token_program.to_account_info(), transfer_accounts_options);
 
-    transfer_checked(cpi_context, *amount, mint.decimals)?;
-    
-    
+    transfer_checked(cpi_context, *amount, mint.decimals)
 }
